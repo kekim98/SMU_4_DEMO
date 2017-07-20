7c5f9768616cc494aab4a687dadfaa9989892475
jngmui <wjdal208@gmail.com>
2017-07-20 15:48:21 +0900

    wjdal208 : 깃 테스트

--- app/src/main/java/example/com/smu_4_demo/MainActivity.java
+++ app/src/main/java/example/com/smu_4_demo/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.input_main);
-        Log.d(TAG, "hi");
+        Log.d(TAG, "안녕하세용 ~~");
 }
 }
