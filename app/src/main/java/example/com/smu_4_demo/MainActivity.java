1a2213ff027f48e31b96a079f810a5e33e40935e
jtiger0303 <jtigerjh0303@gmail.com>
2017-07-20 16:28:35 +0900

    test2

--- app/src/main/java/example/com/smu_4_demo/MainActivity.java
+++ app/src/main/java/example/com/smu_4_demo/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.input_main);
         Log.d(TAG, "HI~");
+        Log.d(TAG, "test2");
 }
 }
