2478909adbb3ec42664e79fc34767c2cbc6da245
jngmui <wjdal208@gmail.com>
2017-07-20 14:47:37 +0900

    wjdal208 : 테스트 로그 삽입

--- app/src/main/java/example/com/smu_4_demo/MainActivity.java
+++ app/src/main/java/example/com/smu_4_demo/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.d(TAG, "test");
-    }
+        setContentView(R.layout.input_main);
+        Log.d(TAG, "hi");
+}
 }
